9bf0ca2c0fcb0d76335ace43f05cc48de025e8ec
argelhunos <52228971+argelhunos@users.noreply.github.com>
2024-08-28 19:30:30 -0400

    Add SafeAreaView and cleanup API response

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import getNextService from './services/apiService';
 import { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 export default function App() {
   const [loading, setLoading] = useState(false);
@@ -21,19 +27,21 @@ export default function App() {
   }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      {loading ? "" : 
-        tripTimes.map((trip, index) => (
-          <View>
-            <Text>{trip.ScheduledDepartureTime}</Text>
-            <Text>{trip.ScheduledPlatform}</Text>
-            <Text>{trip.DirectionName}</Text>
-          </View>
-        ))
-      }
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text>Hello world!</Text>
+        {loading ? "" : 
+          tripTimes.map((trip, index) => (
+            <View key={trip.ScheduledDepartureTime}>
+              <Text>{trip.ScheduledDepartureTime}</Text>
+              <Text>{trip.ScheduledPlatform}</Text>
+              <Text>{trip.DirectionName}</Text>
+            </View>
+          ))
+        }
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
--- services/apiService.js
+++ services/apiService.js
@@ -5,7 +5,11 @@ async function getNextService(stopCode = "mo") {
     try {
         const response = await fetch(`${BASE_URL}/api/V1/Stop/NextService/${stopCode}?key=${KEY}`);
         const data = await response.json();
-        return data["NextService"]["Lines"];  
+        return data["NextService"]["Lines"].map(line => ({
+            ...line, // copy all old assets
+            ScheduledDepartureTime: line.ScheduledDepartureTime.split(' ')[1].substring(0, 5),
+            DirectionName: line.DirectionName.substring(5),
+        }));  
     } catch (error) {
         throw new Error("An error has occurred: " + error);
     }
