0b0674ad2ee8f3bcf1d318e2d9c04d603d5ba7bc
argelhunos <52228971+argelhunos@users.noreply.github.com>
2024-08-28 18:55:38 -0400

    Restructure API call and add basic UI test in App component

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import getNextService from './services/apiService';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [tripTimes, setTripTimes] = useState([]);
+
   useEffect(() => {
+    setLoading(true);
     getNextService()
+      .then(data => {
+        setTripTimes(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      })
   }, [])
 
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
+      {loading ? "" : 
+        tripTimes.map((trip, index) => (
+          <View>
+            <Text>{trip.ScheduledDepartureTime}</Text>
+            <Text>{trip.ScheduledPlatform}</Text>
+            <Text>{trip.DirectionName}</Text>
+          </View>
+        ))
+      }
       <StatusBar style="auto" />
     </View>
   );
@@ -20,7 +41,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- services/apiService.js
+++ services/apiService.js
@@ -1,10 +1,13 @@
 const BASE_URL = "https://api.openmetrolinx.com/OpenDataAPI"
+const KEY = process.env.EXPO_PUBLIC_API_KEY
 
 async function getNextService(stopCode = "mo") {
-    await fetch(`${BASE_URL}/api/V1/Stop/NextService/${stopCode}`)
-        .then(response => response.json())
-        .then(data => { console.log(data) })
-        .catch(error => console.log("an error has occured" + error))
+    try {
+        const response = await fetch(`${BASE_URL}/api/V1/Stop/NextService/${stopCode}?key=${KEY}`);
+        const data = await response.json();
+        return data["NextService"]["Lines"];  
+    } catch (error) {
+        throw new Error("An error has occurred: " + error);
+    }
 }
-
 export default getNextService
