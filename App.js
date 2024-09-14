7cf346929fb73a023e4c8e568caf1e6e37bdd2b4
argelhunos <52228971+argelhunos@users.noreply.github.com>
2024-09-13 22:08:22 -0400

    Customize tab navigator, add scroll to refresh

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import DepartureBoard from './screens/DepartureBoard';
 import Settings from './screens/Settings';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { HelloWidgetPreviewScreen } from './screens/HelloWidgetPreviewScreen';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -12,7 +13,25 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
           <Tab.Navigator
-            screenOptions={{ headerShown: false}}
+            screenOptions={({ route }) => ({ 
+              headerShown: false,
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+
+                if (route.name === "Departure Board") {
+                  iconName = "departure-board";
+                } else if (route.name === "Settings") {
+                  iconName = "settings"
+                } else {
+                  iconName = "settings"
+                }
+
+                // return icon woo
+                return <MaterialIcons name={iconName} size={size} color={color}/>
+              },
+              tabBarActiveTintColor: '#3A8446',
+              tabBarInactiveTintColor: 'gray',
+            })}
           >
             <Tab.Screen name="Departure Board" component={DepartureBoard} />
             <Tab.Screen name="Settings" component={Settings} />
--- screens/DepartureBoard.js
+++ screens/DepartureBoard.js
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, ActivityIndicator, View, Text } from 'react-native';
+import { ScrollView, StyleSheet, ActivityIndicator, View, Text, RefreshControl } from 'react-native';
 import { getNextService, getSchedule } from '../services/apiService.js'
 import { useCallback, useState } from 'react';
 import LineName from '../components/LineName';
@@ -12,50 +12,82 @@ import { useFocusEffect } from '@react-navigation/native';
 function DepartureBoard({ navigation }) {
     const [loading, setLoading] = useState(false);
     const [tripTimes, setTripTimes] = useState([]);
+    const [refreshing, setRefreshing] = useState(false);
     const [line, setLine] = useState("");
     const [stop, setStop] = useState("");
     const insets = useSafeAreaInsets();
 
+    const loadDepartures = () => {
+      setLoading(true);
+
+      getItem('line')
+        .then(data => {
+          setLine(data);
+        });
+      
+      getItem('stop')
+        .then(data => {
+          setStop(data);
+        });
+
+      getNextService()
+        .then(data => {
+            setTripTimes(data);
+            setLoading(false);
+            setRefreshing(false);
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoading(false);
+            setRefreshing(false);
+      })
+    }
+
     // useFocusEffect instead of useEffect to update during each focus, so it is always using updated user line/stop preferences
     useFocusEffect(
       useCallback(() => {
+        let oldUserLine;
+        let oldUserStop;
+
         // if there is no line set, force user to go to settings
         getItem('line')
           .then(data => {
             if (!data) {
               navigation.navigate('Settings');
             }
+
+            // temporarily store for checking for changes with settings
+            
+            oldUserLine = data;
+
             return getItem('stop');
           })
           .then(data => {
+
+            oldUserStop = data;
+
             if (!data) {
               navigation.navigate('Settings');
             }
-          });
-
-        setLoading(true);
-
-        getItem('line')
-          .then(data => {
-            setLine(data);
-          });
-        
-        getItem('stop')
-          .then(data => {
-            setStop(data);
-          });
-
-        getNextService()
-          .then(data => {
-              setTripTimes(data);
-              setLoading(false);
           })
-          .catch((error) => {
-              console.log(error);
-              setLoading(false);
-        })
+          .finally(() => {
+            console.log("old :" + oldUserLine);
+            console.log("old :" + oldUserStop);
+            console.log("new :" + stop);
+            console.log("new :" + line);
+            if (oldUserLine === line && oldUserStop === stop) {
+              return;
+            } else {
+              loadDepartures(); 
+            }
+          })
       }, [])
     );
+
+    const onRefresh = () => {
+      setRefreshing(true);
+      loadDepartures();
+    }
 
     return (
       <View style={{
@@ -74,7 +106,11 @@ function DepartureBoard({ navigation }) {
             lineAbbreviation={lineAbbreviation.get(line)}
             lineColour={lineColour.get(line)}
           />
-          <ScrollView>
+          <ScrollView
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>    
+            }
+          >
             {loading ? <ActivityIndicator size="large" /> : 
               tripTimes.length != 0 ?
               tripTimes.map((trip, index) => (
