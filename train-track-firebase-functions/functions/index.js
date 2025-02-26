63e08f8982fb9b3f452aba64d5e6645b096da2e7
argelhunos <52228971+argelhunos@users.noreply.github.com>
2025-02-26 18:33:07 -0500

    implement minimal implementation of firebase function backend

--- train-track-firebase-functions/functions/index.js
+++ train-track-firebase-functions/functions/index.js
@@ -14,11 +14,70 @@ const { getMessaging } = require("firebase-admin/messaging");
 
 // get train api key for local testing
 require("dotenv").config();
+const BASE_URL = "https://api.openmetrolinx.com/OpenDataAPI";
 const TRAIN_API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
 initializeApp();
 
-//
+function lineTimeCompare(lineA, lineB) {
+    // must convert everything to minutes since midnight for proper handling of past midnight
+    const [lineAHours, lineAMins] = lineA.DisplayedDepartureTime.split(":");
+    const [lineBHours, lineBMins] = lineB.DisplayedDepartureTime.split(":");
+
+    let lineAMinsSinceMidnight = parseInt(lineAHours) * 60 + parseInt(lineAMins);
+    let lineBMinsSinceMidnight = parseInt(lineBHours) * 60 + parseInt(lineBMins);
+
+    // consider times 00:00-03:00 to be greater
+    if (lineAHours === "00" || lineAHours == "01" || lineAHours == "02" || lineAHours == "03" ) {
+        lineAMinsSinceMidnight += 24 * 60;
+    }
+
+    if (lineBHours === "00" || lineBHours == "01" || lineBHours == "02" || lineBHours == "03" ) {
+        lineBMinsSinceMidnight += 24 * 60;
+    }
+
+    if (lineAMinsSinceMidnight > lineBMinsSinceMidnight) {
+        return 1;
+    } else if (lineAMinsSinceMidnight < lineBMinsSinceMidnight) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+// function to retrieve next departure from api
+async function getNextService(stopCode, userLine) {
+    try {
+        console.log(`${BASE_URL}/api/V1/Stop/NextService/${stopCode}?key=${TRAIN_API_KEY}`);
+        const response = await fetch(`${BASE_URL}/api/V1/Stop/NextService/${stopCode}?key=${TRAIN_API_KEY}`);
+        let data = await response.json();
+
+        // Check if "Lines" is null or undefined
+        if (!data["NextService"] || !data["NextService"]["Lines"]) {
+            return [];
+        }
+
+        // if "Lines" is null, there are no departures found.
+        return data["NextService"]["Lines"]
+            .filter((line) => line.LineName === userLine)
+            .map(line => {
+                const newScheduledDepartureTime = line.ScheduledDepartureTime.split(' ')[1].substring(0, 5);
+                const newComputedDepartureTime = line.ComputedDepartureTime.split(' ')[1].substring(0, 5);
+
+                return {
+                    ...line, // copy all old assets
+                    DisplayedDepartureTime: newComputedDepartureTime > newScheduledDepartureTime ? newComputedDepartureTime : newScheduledDepartureTime,
+                    DirectionName: line.DirectionName.substring(5),
+                    Delayed: newComputedDepartureTime > newScheduledDepartureTime,
+                    DisplayedPlatform: line.ActualPlatform === "" ? `Platform ${line.ScheduledPlatform}` : `Platform ${line.ActualPlatform}`,
+                }
+            }
+        ).sort(lineTimeCompare); // sort departures by time  
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
 
 exports.fireNotifications = onRequest(async (req, res) => {
     try {
@@ -34,18 +93,53 @@ exports.fireNotifications = onRequest(async (req, res) => {
             return res.json({ result: "No active notifications found"})
         }
 
+
+        // NOTE TO SELF: forEach does not respect async functions.
         // getting notification data for each, and building the payload to send to correct token
-        snapshot.forEach(async (doc) => {
+
+        for (const doc of snapshot.docs) {
             const notification = doc.data();
-            const docRef = await notification.docRef.get();
-            const fcmToken = await docRef.get();
-
+            const fcmToken = notification.fcmToken;
+            console.log(fcmToken);
+            const nextService = await getNextService(notification.station, notification.line);
+            console.log(nextService[0]);
             const message = {
-
+                data: {
+                    test: JSON.stringify(nextService[0]),
+                },
+                token: fcmToken
             }
-        });
+
+            getMessaging().send(message)
+                .then((response) => {
+                    console.log('Successfully sent message:', response);
+                })
+                .catch((error) => {
+                    console.log('Error sending message:', error);
+                })
+        }
+        // snapshot.forEach(async (doc) => {
+        //     const notification = doc.data();
+        //     const fcmToken = await notification.docRef.get();
+        //     const nextService = await getNextService();
+
+        //     const message = {
+        //         data: {
+        //             test: nextService[0],
+        //             token: fcmToken
+        //         }
+        //     }
+
+        //     getMessaging().send(message)
+        //         .then((response) => {
+        //             console.log('Successfully sent message:', response);
+        //         })
+        //         .catch((error) => {
+        //             console.log('Error sending message:', error);
+        //         })
+        // });
         
-        res.json({ result });
+        res.json({ result: 'completed sending notifications' });
     } catch (error) {
         res.json({result: `error occured while firing notifications: ${error}`})
     }
